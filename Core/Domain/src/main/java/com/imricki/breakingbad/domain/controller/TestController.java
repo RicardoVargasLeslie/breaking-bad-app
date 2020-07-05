949b0ef26fb18e1156923dd253fd26b3a3f71a59
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-07-05 15:00:11 +0100

    testing rest Template

--- Core/Domain/src/main/java/com/imricki/breakingbad/domain/controller/TestController.java
+++ Core/Domain/src/main/java/com/imricki/breakingbad/domain/controller/TestController.java
@@ -1,10 +1,31 @@
 package com.imricki.breakingbad.domain.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(TestController.BASE_URL)
 public class TestController {
+
+	public static final String BASE_URL = "/api/v1/test";
+
+	private final static Logger LOGGER = LoggerFactory.getLogger(TestController.class);
+
+//	@Bean
+//	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+//		return builder.build();
+//	}
+//
+//	@Bean
+//	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+//		return args -> {
+//			Quote quote = restTemplate.getForObject("https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+//			LOGGER.info(quote.toString());
+//		};
+//	}
 
 	@GetMapping("/hola")
 	public String greeting() {
---
+++ Core/Domain/src/main/java/com/imricki/breakingbad/domain/controller/Value.java
@@ -0,0 +1,32 @@
+package com.imricki.breakingbad.domain.controller;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Value {
+
+	private Long id;
+	private String quote;
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getQuote() {
+		return this.quote;
+	}
+
+	public void setQuote(String quote) {
+		this.quote = quote;
+	}
+
+	@Override
+	public String toString() {
+		return "Value{" + "id=" + this.id + ", quote='" + this.quote + '\'' + '}';
+	}
+
+}
