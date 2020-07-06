41c00763fb5f80a79ee585a6b6e314e51df832d0
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-07-06 16:30:29 +0100

    changed array for list

--- Core/Domain/src/main/java/com/imricki/breakingbad/domain/dto/QuoteWrapper.java
+++ Core/Domain/src/main/java/com/imricki/breakingbad/domain/dto/QuoteWrapper.java
@@ -1,23 +1,24 @@
 package com.imricki.breakingbad.domain.dto;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class QuoteWrapper {
 
-	private ArrayList<Quote> quotes = new ArrayList<>();
+	private List<Quote> quotes;
 
 	public QuoteWrapper() {
 		this.quotes = new ArrayList<>();
 	}
 
-	public ArrayList<Quote> getQuotes() {
+	public List<Quote> getQuotes() {
 		return this.quotes;
 	}
 
-	public void setQuotes(ArrayList<Quote> quotes) {
+	public void setQuotes(List<Quote> quotes) {
 		this.quotes = quotes;
 	}
 
