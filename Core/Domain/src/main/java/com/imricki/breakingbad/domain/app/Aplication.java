cb6447104443bf030cf62f2b6dd5b22a26f7e88d
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-07-07 22:37:39 +0100

    added domain dependenci to UI pom

--- Core/Domain/src/main/java/com/imricki/breakingbad/domain/app/Aplication.java
+++ Core/Domain/src/main/java/com/imricki/breakingbad/domain/app/Aplication.java
@@ -8,16 +8,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.imricki.breakingbad.domain.clientbuilder.Client;
-import com.imricki.breakingbad.domain.dto.Quote;
-
 @SpringBootApplication(scanBasePackages = { "com" })
 public class Aplication {
 
 	private static final Logger log = LoggerFactory.getLogger(Aplication.class);
 
 	@Autowired
-	private Client service;
+	// private Client service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Aplication.class, args);
@@ -27,9 +24,9 @@ public class Aplication {
 	public CommandLineRunner run() throws Exception {
 		return args -> {
 
-			Quote q = this.service.getRandomQuote();
+			// Quote q = this.service.getRandomQuote();
 // en realidad devuelves una lista de un elemento
-			System.err.println(q);
+			// System.err.println(q);
 //			List<Quote> list = this.service.getAllQuotes();
 //			list.forEach(System.out::println);
 
