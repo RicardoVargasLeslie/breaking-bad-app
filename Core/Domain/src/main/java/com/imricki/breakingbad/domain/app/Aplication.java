4f57ee1cd68403dfdfa3d48dbbbc3f2ff2f0d124
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-07-07 17:01:13 +0100

    clean up

--- Core/Domain/src/main/java/com/imricki/breakingbad/domain/app/Aplication.java
+++ Core/Domain/src/main/java/com/imricki/breakingbad/domain/app/Aplication.java
@@ -1,6 +1,5 @@
 package com.imricki.breakingbad.domain.app;
 
-import java.time.Duration;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -9,9 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 import com.imricki.breakingbad.domain.dto.Quote;
 import com.imricki.breakingbad.domain.restclient.RestClientService;
@@ -26,14 +23,6 @@ public class Aplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Aplication.class, args);
-	}
-
-	static final int TIMEOUT = 50000;
-
-	@Bean
-	RestTemplate restTemplateWithConnectReadTimeout() {
-		return new RestTemplateBuilder().setConnectTimeout(Duration.ofMillis(TIMEOUT))
-				.setReadTimeout(Duration.ofMillis(TIMEOUT)).build();
 	}
 
 	@Bean
--- Core/Domain/src/main/java/com/imricki/breakingbad/domain/dto/Episode.java
+++ Core/Domain/src/main/java/com/imricki/breakingbad/domain/dto/Episode.java
@@ -1,9 +1,10 @@
 package com.imricki.breakingbad.domain.dto;
 
 import java.io.Serializable;
-import java.util.ArrayList;
+import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AccessLevel;
 import lombok.Data;
@@ -15,23 +16,30 @@ import lombok.Setter;
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Episode implements Serializable {
-
+	
 	@Getter(value = AccessLevel.NONE)
 	@Setter(value = AccessLevel.NONE)
 	private static final long serialVersionUID = 1L;
-
+	
+	@JsonProperty("id")
 	private int id;
-
+	
+	@JsonProperty("title")
 	private String title;
-
+	
+	@JsonProperty("season")
 	private int season;
-
+	
+	@JsonProperty("episode")
 	private int episode;
-
+	
+	@JsonProperty("air_date")
 	private String air_date;
-
-	private ArrayList<String> characters;
-
+	
+	@JsonProperty("characters")
+	private List<String> characters;
+	
+	@JsonProperty("series")
 	private String series;
 
 }
