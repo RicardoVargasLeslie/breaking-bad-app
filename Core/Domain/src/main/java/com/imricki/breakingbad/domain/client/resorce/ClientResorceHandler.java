6a1740e737ff29c967f6d0dc572313c858082177
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-07-11 23:37:55 +0100

    deleted properties file and refactored resources class

--- Core/Domain/src/main/java/com/imricki/breakingbad/domain/client/resorce/ClientResorceHandler.java
+++ Core/Domain/src/main/java/com/imricki/breakingbad/domain/client/resorce/ClientResorceHandler.java
@@ -1,8 +1,6 @@
 package com.imricki.breakingbad.domain.client.resorce;
 
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
@@ -10,38 +8,29 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @Configuration
-@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @PropertySource("classpath:application.properties")
-@ConfigurationProperties(prefix = "api")
-public class ClientResorceHandler implements InitializingBean {
+public class ClientResorceHandler {
 
 	@Value("${api.baseurl}")
 	public static final String BASE_URL = "";
 
-	public static String ALL_CHARACTERS = "/api/characters";
+	public static final String ALL_CHARACTERS = "/api/characters";
 
-	public static String RANDOM_CHARACTER = "/api/character/random";
+	public static final String RANDOM_CHARACTER = "/api/character/random";
 
-	public static String RANDOM_DEATHS = "/api/deaths/random";
+	public static final String RANDOM_DEATHS = "/api/deaths/random";
 
-	public static String DEATHS_COUNT = "/api/death-count";
+	public static final String DEATHS_COUNT = "/api/death-count";
 
-	public static String ALL_EPISODES = "/api/episodes";
+	public static final String ALL_EPISODES = "/api/episodes";
 
-	public static String ALL_QUOTES;
+	public static final String ALL_QUOTES;
 
-	public static String QUOTES_BY_ID = "/api/quotes/";
+	public static final String QUOTES_BY_ID = "/api/quotes/";
 
-	@Value("${api.random.quote}")
-	public static final String RANDOM_QUOTE = "";
+	public static final String RANDOM_QUOTE = "/api/quote/random";
 
-	public static String ALL_DEATHS = "/api/deaths";
-
-	@Override
-	public void afterPropertiesSet() throws Exception {
-
-		System.out.println("in afterPropertiesSet via @Value: " + ClientResorceHandler.BASE_URL);
-
-	}
+	public static final String ALL_DEATHS = "/api/deaths";
 
 }
