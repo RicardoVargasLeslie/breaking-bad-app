548d8d212a9361be8b8352d0c3f3b9c4f19213e9
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-08-04 15:34:32 +0100

    added fxml view and some anotations

--- Core/UI/src/main/java/com/imricki/breakingbad/ui/controller/QuoteController.java
+++ Core/UI/src/main/java/com/imricki/breakingbad/ui/controller/QuoteController.java
@@ -1,12 +1,33 @@
 package com.imricki.breakingbad.ui.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import org.springframework.stereotype.Controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 @Controller
-public class QuoteController {
+public class QuoteController implements Initializable {
+
+	@Getter
+	@FXML
+	private BorderPane componentView;
+
+	@Getter
+	@FXML
+	private Button buttonview;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+	}
 
 }
