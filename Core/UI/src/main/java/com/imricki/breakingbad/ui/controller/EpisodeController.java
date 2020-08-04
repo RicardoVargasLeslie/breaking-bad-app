6c6664c8afdba40ba2dc10cfbb099af8a0368f32
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-08-04 15:24:26 +0100

    added some lombok anotations

--- Core/UI/src/main/java/com/imricki/breakingbad/ui/controller/EpisodeController.java
+++ Core/UI/src/main/java/com/imricki/breakingbad/ui/controller/EpisodeController.java
@@ -2,6 +2,10 @@ package com.imricki.breakingbad.ui.controller;
 
 import org.springframework.stereotype.Controller;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 @Controller
 public class EpisodeController {
 
