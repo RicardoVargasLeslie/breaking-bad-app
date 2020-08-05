69670a9021e9d0710b19dbc61f1b5d03d5537e50
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-08-05 17:02:56 +0100

    clean up

--- Core/UI/src/main/java/com/imricki/breakingbad/ui/app/StageInitializer.java
+++ Core/UI/src/main/java/com/imricki/breakingbad/ui/app/StageInitializer.java
@@ -1,7 +1,5 @@
 package com.imricki.breakingbad.ui.app;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
@@ -14,24 +12,14 @@ import javafx.stage.Stage;
 @Component
 public class StageInitializer implements ApplicationListener<StageReadyEvent> {
 
-//	//@Value("classpath:/fx/view.fxml")
-//	private Resource chartResource;
-	private ApplicationContext applicationContext;
-
-	@Autowired
 	private QuoteController controller = new QuoteController();
 
-	public StageInitializer(ApplicationContext applicationContext) {
+	public StageInitializer() {
 
-		this.applicationContext = applicationContext;
 	}
 
 	@Override
 	public void onApplicationEvent(StageReadyEvent event) {
-
-//			FXMLLoader fxmlLoader = new FXMLLoader(this.chartResource.getURL());
-//			fxmlLoader.setControllerFactory(aClass -> this.applicationContext.getBean(aClass));
-//			Parent parent = fxmlLoader.load();
 
 		Stage stage = event.getStage();
 		stage.setScene(new Scene(this.controller.getComponentView()));
