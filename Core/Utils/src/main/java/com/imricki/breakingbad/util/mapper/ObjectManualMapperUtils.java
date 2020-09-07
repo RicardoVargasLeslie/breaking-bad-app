eaa082368e88a2738bc69a493ad8d957a2ede574
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-09-07 12:14:05 +0100

    created  manual mapper

---
+++ Core/Utils/src/main/java/com/imricki/breakingbad/util/mapper/ObjectManualMapperUtils.java
@@ -0,0 +1,18 @@
+package com.imricki.breakingbad.util.mapper;
+
+public final class ObjectManualMapperUtils {
+
+	private ObjectManualMapperUtils() {
+
+	}
+
+	public static Object from() {
+
+		return null;
+	}
+
+	public static Object to() {
+
+		return null;
+	}
+}
