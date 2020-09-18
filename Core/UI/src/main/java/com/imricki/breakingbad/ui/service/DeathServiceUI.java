c556aee03fce9647923f8708b389c7a1c48c2c0b
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-09-18 11:26:24 +0100

    addednsome uninplemented method to service layer

--- Core/UI/src/main/java/com/imricki/breakingbad/ui/service/DeathServiceUI.java
+++ Core/UI/src/main/java/com/imricki/breakingbad/ui/service/DeathServiceUI.java
@@ -1,15 +1,29 @@
 package com.imricki.breakingbad.ui.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.imricki.breakingbad.domain.service.DeathClient;
-import com.imricki.breakingbad.domain.service.DeathService;
+import com.imricki.breakingbad.ui.model.DeathModel;
 
 @Service
 public class DeathServiceUI implements DeathService {
 
 	@Autowired
 	private DeathClient service;
+
+	@Override
+	public List<DeathModel> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DeathModel getRandom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
