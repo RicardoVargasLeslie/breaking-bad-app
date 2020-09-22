782379ce140315bf504fb47769906216a5057e50
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-09-22 12:23:03 +0100

    changin some components to test view

--- Core/UI/src/main/java/com/imricki/breakingbad/ui/controller/MainController.java
+++ Core/UI/src/main/java/com/imricki/breakingbad/ui/controller/MainController.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Controller;
 
 import com.imricki.breakingbad.ui.utils.LoaderUtils;
 
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.BorderPane;
@@ -19,29 +17,17 @@ public class MainController implements Initializable {
 	@FXML
 	private BorderPane manViewPane;
 
-	private MainController mainController;
-
-	private ObjectProperty<QuoteController> quoteController;
-
 	public MainController() {
 
 		LoaderUtils.loadView("/fx/mainView.fxml", this);
 	}
 
-	public BorderPane getManViewPane() {
-		return this.manViewPane;
-	}
-
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 
-		this.quoteController = new SimpleObjectProperty<QuoteController>();
 	}
 
-	@FXML
-	private void initialize() {
-
-		this.quoteController.get().injectMainController(this.mainController);
+	public BorderPane getManViewPane() {
+		return this.manViewPane;
 	}
-
 }
--- Core/UI/src/main/java/com/imricki/breakingbad/ui/controller/QuoteController.java
+++ Core/UI/src/main/java/com/imricki/breakingbad/ui/controller/QuoteController.java
@@ -3,6 +3,7 @@ package com.imricki.breakingbad.ui.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.imricki.breakingbad.ui.utils.LoaderUtils;
@@ -11,34 +12,32 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TabPane;
-import lombok.Getter;
 
 @Controller
 public class QuoteController implements Initializable {
 
-	@Getter
 	@FXML
 	private TabPane tabView;
 
+	@Autowired
 	private MainController mainController = new MainController();
 
-	@Getter
 	@FXML
 	private Button buttonview;
 
 	public QuoteController() {
 
 		LoaderUtils.loadView("/fx/tabview.fxml", this);
+
+		this.mainController.getManViewPane().setCenter(this.getTabView());
 	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 
-		this.mainController.getManViewPane().setCenter(this.tabView);
 	}
 
-	public void injectMainController(MainController mainController) {
-		this.mainController = mainController;
+	public TabPane getTabView() {
+		return this.tabView;
 	}
-
 }
