2f777302dc4fee23822b7f659935586523c2ea5d
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-09-24 14:10:00 +0100

    loking ok,added icons

--- Core/UI/src/main/java/com/imricki/breakingbad/ui/controller/QuoteController.java
+++ Core/UI/src/main/java/com/imricki/breakingbad/ui/controller/QuoteController.java
@@ -5,44 +5,45 @@ import java.util.ResourceBundle;
 
 import org.springframework.stereotype.Controller;
 
+import com.imricki.breakingbad.ui.model.QuoteModel;
 import com.imricki.breakingbad.ui.utils.LoaderUtils;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import lombok.Getter;
 
 @Controller
 public class QuoteController implements Initializable {
 
 	@FXML
+	@Getter
 	private BorderPane borderPane;
 
 	@FXML
-	private TableView<?> tableView;
+	private TableView<QuoteModel> tableView;
 
 	@FXML
-	private TableColumn<?, ?> idCol;
+	private TableColumn<QuoteModel, Number> idCol;
 
 	@FXML
-	private TableColumn<?, ?> quoteCol;
+	private TableColumn<QuoteModel, String> quoteCol;
 
 	@FXML
-	private TableColumn<?, ?> authorCol;
+	private TableColumn<QuoteModel, String> authorCol;
 
 	@FXML
-	private TableColumn<?, ?> seriesCol;
+	private TableColumn<QuoteModel, String> seriesCol;
 
 	@FXML
 	private ImageView authorImageView;
 
 	@FXML
-	private Label quote_idLabel;
-
-	@FXML
-	private Label quoteLabel;
-
-	@FXML
-	private Label seriesLabel;
+	private Label quoteLabel, seriesLabel, quote_idLabel;
 
 	public QuoteController() {
 
@@ -52,6 +53,12 @@ public class QuoteController implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+
+		// set cell value factories
+		this.idCol.setCellValueFactory(v -> v.getValue().getQuote_id());
+		this.quoteCol.setCellValueFactory(v -> v.getValue().getQuote());
+		this.authorCol.setCellValueFactory(v -> v.getValue().getAuthor());
+		this.seriesCol.setCellValueFactory(v -> v.getValue().getSeries());
 
 	}
 
