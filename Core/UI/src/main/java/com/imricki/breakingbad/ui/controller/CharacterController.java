e4b3029185ac695a7d9582f6febdfab0a7f88a16
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-11-03 10:04:56 +0000

    deleted some unised methods

--- Core/UI/src/main/java/com/imricki/breakingbad/ui/controller/CharacterController.java
+++ Core/UI/src/main/java/com/imricki/breakingbad/ui/controller/CharacterController.java
@@ -1,27 +1,20 @@
 package com.imricki.breakingbad.ui.controller;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 import java.util.logging.Logger;
 
 import org.springframework.stereotype.Controller;
 
-import javafx.fxml.Initializable;
-
 @Controller
-public class CharacterController implements Initializable {
+public class CharacterController  {
 
 	private static final Logger LOGGER = Logger.getLogger(CharacterController.class.getName());
 
 	public CharacterController() {
 
-		LOGGER.info("Obj Constructor");
+
 
 	}
 
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
 
-	}
 
 }
